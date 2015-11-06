148c5c03577a34486457b6ee8506973439df8fc7
nialbima@gmail.com <nialbima@gmail.com>
2015-11-06 11:13:30 -0500

    converted to open-uri

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,3 @@
-var tagData = [];
-var marker;
-
 function initMap(data) {
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: {lat: 40.7410986, lng: -73.9888682},
@@ -24,24 +21,24 @@ function createMarker(coords, map, title){
     title: title
   });
 }
-
-
-// get location id
-// https://api.instagram.com/v1/locations/search?lat=1&lng=2&access_token=Your-Act
-var query_string = 'https://api.instagram.com/v1/locations/search?lat=40.7410986&lng=-73.9888682&client_id=62b689a145a94aa2863cec35efc57e12'
-var next_max_id  = 1111879330113488870
-var loc_string   = 'https://api.instagram.com/v1/locations/127540101/media/recent?client_id=62b689a145a94aa2863cec35efc57e12'
-
-function getIGdata(){
-  $.ajax({
-    url: 'https://api.instagram.com/v1/locations/search?lat=40.7410986&lng=-73.9888682&client_id=62b689a145a94aa2863cec35efc57e12',
-    crossDomain: true,
-    dataType:'jsonp',
-    method:'get',
-    success: function(response){ console.log(response) }
-  })
-}
-// get recent results from location
-// https://api.instagram.com/v1/locations/{location-id}/media/recent?access_token=<% Rails.secrets.whatever %>
-
-// popular endpoint w/ a set location will get us top-ten results
+//
+//
+// // get location id
+// // https://api.instagram.com/v1/locations/search?lat=1&lng=2&access_token=Your-Act
+// var query_string = 'https://api.instagram.com/v1/locations/search?lat=40.7410986&lng=-73.9888682&client_id=62b689a145a94aa2863cec35efc57e12'
+// var next_max_id  = 1111879330113488870
+// var loc_string   = 'https://api.instagram.com/v1/locations/127540101/media/recent?client_id=62b689a145a94aa2863cec35efc57e12'
+//
+// function getIGdata(){
+//   $.ajax({
+//     url: 'https://api.instagram.com/v1/locations/search?lat=40.7410986&lng=-73.9888682&client_id=62b689a145a94aa2863cec35efc57e12',
+//     crossDomain: true,
+//     dataType:'jsonp',
+//     method:'get',
+//     success: function(response){ console.log(response) }
+//   })
+// }
+// // get recent results from location
+// // https://api.instagram.com/v1/locations/{location-id}/media/recent?access_token=<% Rails.secrets.whatever %>
+//
+// // popular endpoint w/ a set location will get us top-ten results
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,6 +2,39 @@ class MapsController < ApplicationController
 
 
   def show
+    endpoint = "https://api.instagram.com/v1/locations/search?lat=40.7410986&lng=-73.9888682&client_id=62b689a145a94aa2863cec35efc57e12"
+    buffer = open(endpoint, "User-Agent" => "ruby").read
+    result = JSON.parse(buffer)
+
+    @ig_data = result['data']
+
+    binding.pry
+    # just do "open(url){
+    #
+    # }"
+
   end
 
 end
+
+# // // get location id
+# // // https://api.instagram.com/v1/locations/search?lat=1&lng=2&access_token=Your-Act
+# // var query_string = 'https://api.instagram.com/v1/locations/search?lat=40.7410986&lng=-73.9888682&client_id=62b689a145a94aa2863cec35efc57e12'
+# // var next_max_id  = 1111879330113488870
+# // var loc_string   = 'https://api.instagram.com/v1/locations/127540101/media/recent?client_id=62b689a145a94aa2863cec35efc57e12'
+
+
+# open("https://api.instagram.com/v1/locations/search?lat=40.7410986&lng=-73.9888682&client_id=62b689a145a94aa2863cec35efc57e12", "User-Agent" => "ruby") {|f|
+#     f.each_line {|line| p line}
+#     # p f.base_uri         # <URI::HTTP:0x40e6ef2 URL:http://www.ruby-lang.org/en/>
+#     # p f.content_type     # "text/html"
+#     # p f.charset          # "iso-8859-1"
+#     # p f.content_encoding # []
+#     # p f.last_modified    # Thu Dec 05 02:45:02 UTC 2002
+# }
+#
+#
+# open("https://www.ruby-lang.org/en") {|f|
+#   f.each_line {|line| p line}
+#
+# }
